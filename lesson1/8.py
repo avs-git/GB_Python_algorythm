b16a402c0533b27d3460df46b1ec2ab71b88a1b7
avs-git <schmidtav@yandex.ru>
2019-08-01 14:42:06 +0300

    Lesson1 small code review

--- lesson1/8.py
+++ lesson1/8.py
@@ -4,10 +4,7 @@ year = int(input('Введите год: '))
 
 
 def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-
-    return False
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
 
 print(is_leap_year(year))
